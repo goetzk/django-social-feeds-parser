1084647083b4eed1cb60d2964281ab7c15d74531
Karl Goetz <karl@kgoetz.id.au>
2019-05-29 20:13:26 +1000

    Change settings import
    As far as I can tell the previous behaviour was to import 'settings'
    from within the settings module. That meant it was importing
    django.settings via socialfeedsparser.settings and ignoring all
    configuration defined in socialfeedsparser.settings.
    
    I'm not sure if that was ever working as expected.
    
    I've changed that to import socialfeedsparser.settings itself which
    resolves SOCIALFEEDSPARSER_TAG_TEMPLATE resolution errors I'm seeing,
    and appears to fix the settings imports too.

--- socialfeedsparser/templatetags/socialfeedsparser_tags.py
+++ socialfeedsparser/templatetags/socialfeedsparser_tags.py
@@ -6,7 +6,7 @@ from django import template
 from django.utils.safestring import mark_safe
 
 from socialfeedsparser.models import Channel
-from socialfeedsparser.settings import settings
+from socialfeedsparser import settings
 
 register = template.Library()
 
